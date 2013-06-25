08abbc81da352daf0e739209ded40c47d66b18d4
ernado-x <andrew.gubskiy@gmail.com>
2013-06-26 00:07:16 +0300

    Some refactoring

--- CredentialsMembershipProvider.cs
+++ CredentialsMembershipProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.Linq;
 using System.Web.Configuration;
 using System.Web.Security;
 
@@ -7,8 +8,10 @@ namespace X.Web.Security
 {
     public class CredentialsMembershipProvider : MembershipProvider
     {
-        private FormsAuthenticationUserCollection _users = null;
+        private FormsAuthenticationUserCollection _users;
         private FormsAuthPasswordFormat _passwordFormat;
+
+        public const string ProviderName = "CredentialsMembershipProvider";
 
         public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
         {
@@ -16,10 +19,23 @@ namespace X.Web.Security
             _passwordFormat = GetPasswordFormat();
         }
 
+        public override MembershipUser GetUser(string username, bool userIsOnline)
+        {
+            var user = GetUser(username);
+
+            return CreateMembershipUser(user);
+        }
+
+        private static MembershipUser CreateMembershipUser(FormsAuthenticationUser user)
+        {
+            return new MembershipUser(ProviderName, user.Name, null, String.Empty, String.Empty, String.Empty, true,
+                                      false, DateTime.Now.Date, DateTime.Now.Date, DateTime.Now.Date, DateTime.Now.Date,
+                                      DateTime.Now.Date);
+        }
+
         public override bool ValidateUser(string username, string password)
         {
-            var users = GetUsers();
-            var user = users[username];
+            var user = GetUser(username);
 
             if (user == null)
             {
@@ -44,7 +60,14 @@ namespace X.Web.Security
             return false;
         }
 
-        protected FormsAuthenticationUserCollection GetUsers()
+        private FormsAuthenticationUser GetUser(string username)
+        {
+            var users = GetUsers();
+            var user = users[username];
+            return user;
+        }
+
+        private FormsAuthenticationUserCollection GetUsers()
         {
             if (_users == null)
             {
@@ -52,18 +75,20 @@ namespace X.Web.Security
                 FormsAuthenticationCredentials creds = section.Forms.Credentials;
                 _users = section.Forms.Credentials.Users;
             }
+
             return _users;
         }
 
-        protected static AuthenticationSection GetAuthenticationSection()
+        private static AuthenticationSection GetAuthenticationSection()
         {
-            Configuration config = WebConfigurationManager.OpenWebConfiguration("~");
+            var config = WebConfigurationManager.OpenWebConfiguration("~");
             return (AuthenticationSection)config.GetSection("system.web/authentication");
         }
 
-        protected FormsAuthPasswordFormat GetPasswordFormat()
+        private static FormsAuthPasswordFormat GetPasswordFormat()
         {
-            return GetAuthenticationSection().Forms.Credentials.PasswordFormat;
+            var authenticationSection = GetAuthenticationSection();
+            return authenticationSection.Forms.Credentials.PasswordFormat;
         }
 
         public override string ApplicationName { get; set; }
@@ -75,72 +100,78 @@ namespace X.Web.Security
 
         public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public override bool DeleteUser(string username, bool deleteAllRelatedData)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public override bool EnablePasswordReset
         {
-            get { throw new NotImplementedException(); }
+            get { return false; }
         }
 
         public override bool EnablePasswordRetrieval
         {
-            get { throw new NotImplementedException(); }
+            get { return false; }
         }
 
         public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
         {
-            throw new NotImplementedException();
+            var users = GetUsers();
+            
+            var  membershipUserCollection = new MembershipUserCollection();
+
+            foreach (FormsAuthenticationUser user in users)
+            {
+                membershipUserCollection.Add(CreateMembershipUser(user));
+            }
+
+            totalRecords = membershipUserCollection.Count;
+
+            return membershipUserCollection;
         }
 
         public override int GetNumberOfUsersOnline()
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public override string GetPassword(string username, string answer)
         {
-            throw new NotImplementedException();
-        }
-
-        public override MembershipUser GetUser(string username, bool userIsOnline)
-        {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public override string GetUserNameByEmail(string email)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public override int MaxInvalidPasswordAttempts
         {
-            get { throw new NotImplementedException(); }
+            get { return 5; }
         }
 
         public override int MinRequiredNonAlphanumericCharacters
@@ -150,7 +181,7 @@ namespace X.Web.Security
 
         public override int MinRequiredPasswordLength
         {
-            get { throw new NotImplementedException(); }
+            get { return 1; }
         }
 
         public override int PasswordAttemptWindow
@@ -160,37 +191,37 @@ namespace X.Web.Security
 
         public override MembershipPasswordFormat PasswordFormat
         {
-            get { throw new NotImplementedException(); }
+            get { throw new NotSupportedException(); }
         }
 
         public override string PasswordStrengthRegularExpression
         {
-            get { throw new NotImplementedException(); }
+            get { throw new NotSupportedException(); }
         }
 
         public override bool RequiresQuestionAndAnswer
         {
-            get { throw new NotImplementedException(); }
+            get { throw new NotSupportedException(); }
         }
 
         public override bool RequiresUniqueEmail
         {
-            get { throw new NotImplementedException(); }
+            get { throw new NotSupportedException(); }
         }
 
         public override string ResetPassword(string username, string answer)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public override bool UnlockUser(string userName)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public override void UpdateUser(MembershipUser user)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
     }
 }
